b866fcd30e5dda85472e08aaa3f5102c8e1df6d8
Ramon Lobo <ramonlobo@id.uff.br>
2018-08-17 23:34:47 -0300

    Added: Exception handler, Validation, Jobs Queue

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const Raven = require('raven')
+
+const Config = use('Config')
 const Env = use('Env')
 const Youch = use('Youch')
 
@@ -24,16 +27,16 @@ class ExceptionHandler extends BaseExceptionHandler {
    * @return {void}
    */
   async handle (error, { request, response }) {
-    if (error.name == 'ValidationException')
+    if (error.name === 'ValidationException') {
       return response.status(error.status).send(error.messages)
+    }
 
-    if (Env.get('NODE_ENV') == 'development') {
+    if (Env.get('NODE_ENV') === 'development') {
       const youch = new Youch(error, request.request)
       const errorJSON = await youch.toJSON()
 
       return response.status(error.status).send(errorJSON)
     }
-
   }
 
   /**
@@ -47,6 +50,8 @@ class ExceptionHandler extends BaseExceptionHandler {
    * @return {void}
    */
   async report (error, { request }) {
+    Raven.config(Config.get('services.sentry.dsn'))
+    Raven.captureException(error)
   }
 }
 
